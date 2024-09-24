90e6a2008511b36e2336f3787583650b3b85b526
Mrboss1199 <t57havytanks@gmail.com>
2024-09-24 14:03:53 +0700

    EditName

--- homedaddy-storefront/src/app/[countryCode]/(main)/page.tsx
+++ homedaddy-storefront/src/app/[countryCode]/(main)/page.tsx
@@ -8,9 +8,9 @@ import { ProductCollectionWithPreviews } from "types/global"
 import { cache } from "react"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "HomeDaddy",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "New home for everyone",
 }
 
 const getCollectionsWithProducts = cache(
